9f03ae875cf0bfe69aa11eb5b2398c014b58a5f3
vraisinghani <vraisinghani@pivotal.io>
2020-04-29 16:51:55 -0400

    Adding creation of db, initial schema and changes to pipeline.

---
+++ databases/tracker/create_databases.sql
@@ -0,0 +1,20 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+DROP DATABASE IF EXISTS tracker_dev;
+DROP DATABASE IF EXISTS tracker_test;
+
+CREATE DATABASE tracker_dev;
+CREATE DATABASE tracker_test;
+
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT
+      FROM   pg_catalog.pg_roles
+      WHERE  rolname = 'tracker') THEN
+
+      CREATE ROLE tracker LOGIN ENCRYPTED PASSWORD 'password';
+   END IF;
+END
+$do$;
